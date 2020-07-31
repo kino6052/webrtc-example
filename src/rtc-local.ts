c1dd9873faf649a04bcf5b538b8840b6ff94715d
Kirill Novik <kino6052@colorado.edu>
2020-07-30 17:41:38 -0700

    Add Small Changes

--- src/rtc-local.ts
+++ src/rtc-local.ts
@@ -5,10 +5,16 @@ import { Client } from "./client";
 const init = () => {
   const client1 = new Client();
   const client2 = new Client();
+  const client3 = new Client();
+  const client4 = new Client();
   // @ts-ignore
   window.client1 = client1;
   // @ts-ignore
   window.client2 = client2;
+  // @ts-ignore
+  window.client3 = client3;
+  // @ts-ignore
+  window.client4 = client4;
 };
 
 InitSubject.pipe(
